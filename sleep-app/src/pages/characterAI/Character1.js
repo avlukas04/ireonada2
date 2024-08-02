c94ea09048f65d624d865e7b1647abb5950c9d04
Avneet Thind <athind@berkeley.edu>
2024-08-01 18:09:12 -0700

    basic

--- sleep-app/src/pages/characterAI/Character1.js
+++ sleep-app/src/pages/characterAI/Character1.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Character1() {
   return (
     <div>
-      <p>character page</p>
+      <h3>Welcome, User</h3>
+      <p>Enter a description and press 'Generate' to generate your AI companion</p>
+      // button that says generate
+      // input text box
+      //noggin work
+      //output display
+      //press OK and continue to main AI character page
     </div>
   )
 }
