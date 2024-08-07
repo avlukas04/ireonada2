55891013d0ab68dcc873d218d288700f9da773b4
Liwen Sun <liwensun@berkeley.edu>
2024-08-07 09:47:54 -0700

    small changes

--- sleep-app/src/components/overall/TopBar.js
+++ sleep-app/src/components/overall/TopBar.js
@@ -63,7 +63,7 @@ function TopBar({ imageUrl }) {
                             variant="h5"
                             noWrap
                             component="a"
-                            href="#app-bar-with-responsive-menu"
+                            href="/"
                             sx={{
                                 mr: 2,
                                 display: { xs: 'flex', md: 'none' },
